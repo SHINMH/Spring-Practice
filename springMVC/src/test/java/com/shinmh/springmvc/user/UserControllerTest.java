17bf5d2c1184f209eeb95554d9a8a1f88c27e9c4
SHINMH <sky95012@gmail.com>
2022-04-29 15:28:55 +0900

    springMVC 연습

---
+++ springMVC/src/test/java/com/shinmh/springmvc/user/UserControllerTest.java
@@ -0,0 +1,30 @@
+package com.shinmh.springmvc.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello"));
+    }
+
+    @Test
+    public void createUser_JSON() throws Exception {
+        mockMvc.perform(post("/user/create"))
+                .andExpect()
+    }
+}
