f00d74fde00b2238d90395e959d3b88060449d0e
SHINMH <sky95012@gmail.com>
2022-05-02 13:57:47 +0900

    Add user class and Test user method

--- springMVC/src/test/java/com/shinmh/springmvc/user/UserControllerTest.java
+++ springMVC/src/test/java/com/shinmh/springmvc/user/UserControllerTest.java
@@ -3,12 +3,14 @@ package com.shinmh.springmvc.user;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserController.class)
 public class UserControllerTest {
@@ -24,7 +26,13 @@ public class UserControllerTest {
 
     @Test
     public void createUser_JSON() throws Exception {
-        mockMvc.perform(post("/user/create"))
-                .andExpect()
+        String userJson = "{\"username\":\"shinmh\", \"password\":\"1234\"}";
+        mockMvc.perform(post("/users/create")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .content(userJson))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.username", is(equalTo("shinmh"))))
+                    .andExpect(jsonPath("$.password", is(equalTo("1234"))));
     }
 }
