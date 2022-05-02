ae5e029d2cca4acf9975bb6281bcf3fabf66a423
SHINMH <sky95012@gmail.com>
2022-05-02 17:06:17 +0900

    Add HtmlUnit Test

---
+++ TemplateEngineProject/src/test/java/com/shinmh/templateengineproject/SampleControllerTest.java
@@ -0,0 +1,35 @@
+package com.shinmh.templateengineproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
+
+@WebMvcTest
+public class SampleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        // 요청 "/"
+        // 응답
+        // - 모델 name : shinmh
+        // - 뷰 이름 :  hello
+
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("name", is("shinmh")))
+                .andExpect(content().string(containsString("shinmh")));
+    }
+
+}
--- TemplateEngineProject/src/test/java/com/shinmh/templateengineproject/SampleControllerTest2.java
+++ TemplateEngineProject/src/test/java/com/shinmh/templateengineproject/SampleControllerTest2.java
@@ -1,21 +1,26 @@
 package com.shinmh.templateengineproject;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @WebMvcTest
-public class SmapleControllerTest {
+public class SampleControllerTest2 {
 
     @Autowired
-    MockMvc mockMvc;
+    WebClient webClient;
 
     @Test
     public void hello() throws Exception {
@@ -24,12 +29,9 @@ public class SmapleControllerTest {
         // - 모델 name : shinmh
         // - 뷰 이름 :  hello
 
-        mockMvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andDo(print())
-                .andExpect(view().name("hello"))
-                .andExpect(model().attribute("name", is("shinmh")))
-                .andExpect(content().string(containsString("shinmh")));
+        HtmlPage page = webClient.getPage("/hello");
+        HtmlHeading1 h1 = page.getFirstByXPath("//h1");
+        assertThat(h1.getTextContent()).isEqualToIgnoringCase("shinmh");
     }
 
 }
